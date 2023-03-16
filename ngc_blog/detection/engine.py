5d2bb7e1371cdf11651257d17042f7936c894e1b
andrew <inter>
2023-03-16 23:00:07 +0000

    update training

--- ngc_blog/detection/engine.py
+++ ngc_blog/detection/engine.py
@@ -52,7 +52,7 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
 
         metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
-        break
+
     return metric_logger
 
 
